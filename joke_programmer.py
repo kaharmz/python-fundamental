11ec96118560418ff48d892f420b0abab5730615
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-05 17:40:54 +0700

    initial commit

---
+++ joke_programmer.py
@@ -0,0 +1,22 @@
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka budi berangkat ke toko")
+print("Dan mulai belanja")
+
+#Percabangan
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jumlah telur {jumlah_telur} butir")
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 butir telur")
+else:
+    print("Budi tidak jadi beli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
