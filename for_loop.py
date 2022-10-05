a7ae34664b412ad4a9fb55bc856f9b0adbe94f5d
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-05 18:26:51 +0700

    While loop and for implementation

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+"""
+Looping application for reading book with for
+"""
+
+total_book = 10
+print('Ibu said, "Read your book"')
+total_read = 0
+
+print(f"Total already read {total_read}")
+for total_read in range(1, total_book + 1):
+    print(f"the book {total_read} is already read")
+
+print(f"Total already read {total_read}")
---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#While loop
+total_book = 10
+print('Ibu said, "Read your book"')
+
+total_read = 0
+print(f"Total already read {total_read}")
+
+while total_read < total_book:
+    total_read = total_read + 1
+    print(f"Book to-{total_read} already read")
+print(f"Total already read {total_book}")
+
