bbc1c536126dd52431f02e6089014c105e9e5edb
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-10 20:35:13 +0700

    Merger Two List using zip function

---
+++ list.py
@@ -0,0 +1,41 @@
+food = [
+    'Ayam goreng',
+    'Nasi Goreng',
+    'Pecel Ayam',
+    'Burger',
+    'Pizza',
+    'Kebab',
+    'Sate ayam',
+    'Bubur ayam',
+    'Gorengan',
+    'Lontong sayur',
+    'Siomay',
+]
+
+price = [
+    '21000',
+    '14000',
+    '18000',
+    '45000',
+    '87000',
+    '25000',
+    '26000',
+    '12000',
+    '2500',
+    '15000',
+    '17000'
+]
+
+print("================= Food menu =================")
+
+merger = [
+    list(i)
+    for i in zip(food, price)
+]
+
+for j in merger:
+    print(j)
+
+
+
+
