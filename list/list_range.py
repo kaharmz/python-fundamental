f11bad321c9303e03eaeb094559551ec908ff224
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-14 20:56:44 +0700

    Add range, slicing list and copy list

--- list/list_range.py
+++ list/list_range.py
@@ -13,4 +13,26 @@ other_squares = squares[1:10]
 for j in other_squares:
     print(j)
 
+#merge two list
+programming_languages = [
+    'Python',
+    'Javascript',
+    'Typescript',
+    'Java',
+    'Kotlin',
+    'Golang',
+    'R',
+    'Cobol',
+    'C#',
+    'C++',
+    'Php'
+]
+
+used = ['99%', '80', '70', '60', '50', '40', '30', '20', '10']
+popularity = [list(i) for i in zip(programming_languages, used)]
+
+#3 popular in the world
+for j in popularity[0:3]:
+    print(j)
+
 
---
+++ number/numbers.py
@@ -0,0 +1,9 @@
+import this
+
+x, y, z = 12, 14, 10
+
+print((x + z) * y)
+
+#Constant
+MAX_CONNECTIONS = 2000000
+
