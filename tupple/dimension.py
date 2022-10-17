8b539587c1849f9ebcbe021f307967579162bee0
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-17 20:20:14 +0700

    Tuple

---
+++ tupple/dimension.py
@@ -0,0 +1,9 @@
+#Nested tuple
+integers_one = (1, 8, 12, 13, 20)
+
+integers_two = (2, 23, 12, 9, 7)
+
+for i in integers_one:
+    for j in integers_two:
+        if i == j:
+            print(i, j)
