1dfbd1af3102701e9c77d81da23cc79120fb1fed
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-19 19:53:00 +0700

    Conditional if elif and else

---
+++ if/cost.py
@@ -0,0 +1,10 @@
+#Discount example
+
+age = 12
+
+if age < 5:
+    print('Your admission cost is $0.')
+elif age < 18:
+    print('Your admission cost is $25')
+else:
+    print('YOur admission cost is $40.')
