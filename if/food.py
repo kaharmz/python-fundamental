aea4a7e5e2f70adf6dd6c5300939531a91dbe70a
kaharmz <abdkaharmuzakir@gmail.com>
2022-10-21 19:48:43 +0700

    Refactoring code

--- if/food.py
+++ if/food.py
@@ -1,45 +1,44 @@
 
-foods = ['Pecel lele', 'Nasi goreng', 'Gado-gado']
-
 choice = input(f"Select menu : ")
 
+foods = ['Pecel lele', 'Ayam goreng', 'Ikan goreng']
+
 inputs = int(input("Enter quantity: "))
+
+discount = 10000
 
 if choice == foods[0]:
     if inputs < 6:
         price = 16000
         total = inputs * price
         print(f'{foods[0]} == {price}')
-        print(f'Total amount = {foods[0]} x {inputs} = {total}')
+        print(f'Order quantity = {foods[0]} x {inputs} = {total}')
     elif inputs > 6:
         price = 16000
-        discount = 10000
         total = (inputs * price) - discount
-        print(f'Total amount = {foods[0]} x {inputs} - {discount} = {total}')
+        print(f'Order quantity = {foods[0]} x {inputs} - {discount} = {total}')
 elif choice == foods[1]:
     if inputs < 6:
-        price = 14000
+        price = 20000
         total = inputs * price
         print(f'{foods[1]} == {price}')
-        print(f'Total amount = {foods[1]} x {inputs} = {total}')
+        print(f'Order quantity = {foods[1]} x {inputs} = {total}')
     elif inputs > 6:
-        price = 14000
-        discount = 5000
-        total = inputs * price - discount
+        price = 20000
+        total = (inputs * price) - discount
         print(f'{foods[1]} == {price}')
-        print(f'Total amount = {foods[1]} x {inputs} - {discount} = {total}')
+        print(f'Order quantity = {foods[1]} x {inputs} - {discount} = {total}')
 elif choice == foods[2]:
     if inputs < 6:
-        price = 15000
+        price = 25000
         total = inputs * price
         print(f'{foods[2]} == {price}')
-        print(f'Total amount = {foods[2]} x {inputs} = {total}')
+        print(f'Order quantity = {foods[2]} x {inputs} = {total}')
     elif inputs > 6:
-        price = 14000
-        discount = 3000
-        total = inputs * price - discount
+        price = 25000
+        total = (inputs * price) - discount
         print(f'{foods[2]} == {price}')
-        print(f'Total amount = {foods[2]} x {inputs} - {discount} = {total}')
+        print(f'Order quantity = {foods[2]} x {inputs} - {discount} = {total}')
 else:
     print("Please select menu :)")
 
